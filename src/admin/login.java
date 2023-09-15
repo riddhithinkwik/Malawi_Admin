09d2b64ec4b73ebdcaada26c4f94d7822a527ae1
riddhithinkwik <riddhi.p@thinkwik.com>
2023-09-15 17:41:17 +0530

    [feat] Create login page

--- src/admin/login.java
+++ src/admin/login.java
@@ -1,9 +1,13 @@
 package admin;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
 
 @Test
@@ -11,15 +15,30 @@ public class login {
 
 	static WebDriver driver;
 
-@BeforeTest
+@Test
 	public void openAdminportal() {
 
-		System.setProperty("webdriver.chrome.driver", "/Users/krunal/Downloads/chromedriver_mac64_new/chromedriver");
-
+		System.setProperty("webdriver.chrome.driver", "/Users/krunal/Desktop/chromedriver-mac-x64/chromedriver");
+		System.out.println("Open Browser");
 		driver = new ChromeDriver();
-
-		driver.get("https://www.mei.org.mw/contact_us.html");
+		
+		driver.get("http://192.168.1.32:5173/login");
 		driver.manage().window().maximize();
 
 	}
+
+@AfterTest
+public void loginpage() {
+	WebElement email = driver.findElement(By.id("email"));
+	email.sendKeys("admin@mei.org.mw");
+	
+	WebElement password = driver.findElement(By.id("password"));
+	password.sendKeys("1234567890");
+	
+	WebElement loginbtn  = driver.findElement(By.id(":r2:"));
+	loginbtn.click();
+	
+	
+}
+
 }
